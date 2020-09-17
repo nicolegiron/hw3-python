e60f8cc01faa4d0fc9477cbf8727673c3ec01856
Nicole Giron <nqg5259@psu.edu>
2020-09-17 20:21:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#Author : Nicole Giron nqg5259@psu.edu
+
+def digit_sum(n):
+  if n == 0:
+    return 0
+  else:
+    return n % 10 + digit_sum(int(n/10))
+
+def run():
+  n = int(input("Enter an int: "))
+  print(f"sum of digits {n} is {digit_sum(n)}.")
+
+if __name__ == "__main__":
+  run()
